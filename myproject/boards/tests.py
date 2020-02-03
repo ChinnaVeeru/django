f67d3ea261fe745506320a8c4d0515cbb1226bd5
“ChinnaVeeru” <chinnaeeranna@gmail.com>
2020-02-03 11:23:19 +0530

    Commit message.

--- myproject/boards/tests.py
+++ myproject/boards/tests.py
@@ -1,4 +1,9 @@
 #from django.core.urlresolvers import reverse
+#from django.urls import resolve
+#from django.test import TestCase
+from .views import home, board_topics
+from .models import Board
+
 from django.urls import reverse
 from django.urls import resolve
 from django.test import TestCase
@@ -13,3 +18,26 @@ class HomeTests(TestCase):
     def test_home_url_resolves_home_view(self):
         view = resolve('/')
         self.assertEquals(view.func, home)
+
+class HomeTests(TestCase):
+    def setUp(self):
+        self.board = Board.objects.create(name='Django', description='Django board.')
+        url = reverse('home')
+        self.response = self.client.get(url)
+
+    def test_home_view_status_code(self):
+        self.assertEquals(self.response.status_code, 200)
+
+    def test_home_url_resolves_home_view(self):
+        view = resolve('/')
+        self.assertEquals(view.func, home)
+
+    def test_home_view_contains_link_to_topics_page(self):
+        board_topics_url = reverse('board_topics', kwargs={'pk': self.board.pk})
+        self.assertContains(self.response, 'href="{0}"'.format(board_topics_url))
+
+    def test_board_topics_view_contains_link_back_to_homepage(self):
+        board_topics_url = reverse('board_topics', kwargs={'pk': 1})
+        response = self.client.get(board_topics_url)
+        homepage_url = reverse('home')
+        self.assertContains(response, 'href="{0}"'.format(homepage_url))
--- myproject/boards/views.py
+++ myproject/boards/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
-
-# Create your views here.
-
-
-from django.http import HttpResponse
+from django.http import Http404
 from .models import Board
 
 def home(request):
+    # code suppressed for brevity
     boards = Board.objects.all()
     return render(request, 'home.html', {'boards': boards})
+
+def board_topics(request, pk):
+    try:
+        board = Board.objects.get(pk=pk)
+    except Board.DoesNotExist:
+        raise Http404
+    return render(request, 'topics.html', {'board': board})
